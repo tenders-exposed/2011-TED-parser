bf2327c690b28a9d546ab38135db4d1178d9e4d8
zufanka <adriana@homolova.sk>
2015-11-04 11:51:00 +0100

    fixed parser

--- 2011-TED-xmlparser.py
+++ 2011-TED-xmlparser.py
@@ -3,8 +3,22 @@
 
 from bs4 import BeautifulSoup as bs
 import glob, os, re, csv
+from slugify import slugify
+from datetime import datetime
 
+startTime = datetime.now()
+
+#PYTHON: glob module http://stackoverflow.com/questions/18262293/python-open-every-file-in-a-folder
+#VIM: comment multiple lines http://unix.stackexchange.com/questions/120615/how-to-comment-multiple-lines-at-once
+#VIM: comment more lines at the same time http://stackoverflow.com/questions/9549729/in-vim-how-do-i-effectively-insert-the-same-characters-across-multiple-lines
+
+testfile = open("repairkit/tmp.xml").read()
 xmlnum = 0
+
+# oanda.com http://www.oanda.com/currency/average?amount=1&start_month=1&start_year=2011&end_month=12&end_year=2011&base=EUR&avg_type=Year&Submit=1&exchange=BGN&exchange=CHF&exchange=CYP&exchange=CZK&exchange=DKK&exchange=EEK&exchange=GBP&exchange=HUF&exchange=ISK&exchange=JPY&exchange=LTL&exchange=LVL&exchange=MKD&exchange=MTL&exchange=NOK&exchange=PLN&exchange=RON&exchange=SEK&exchange=SKK&exchange=TRY&exchange=USD&interbank=0&format=CSV
+
+exchangefile = csv.reader(open("oneeuro.csv"))
+exchangerates = {r[0] : r[1] for r in exchangefile}
 
 def parse(xmlfile):
 
@@ -26,10 +40,27 @@ def parse(xmlfile):
         except AttributeError:
             return ""
 
+    def iferror2alt(tagname,tagname2,alttagname,alttagname2):
+
+        try:
+            return soup.find(tagname).find(tagname2).text.encode("utf-8")
+        except AttributeError:
+            try:
+                return soup.find(alttagname).find(alttagname2).text.encode("utf-8")
+            except AttributeError:
+                return ""
+
     def iferror2(tagname,tagname2):
 
         try:
             return soup.find(tagname).find(tagname2).text.encode("utf-8")
+        except AttributeError:
+            return ""
+
+    def iferror3get(tagname,tagname2,tagname3,attrib):
+
+        try:
+            return soup.find(tagname).find(tagname2).find(tagname3).get(attrib)
         except AttributeError:
             return ""
 
@@ -47,18 +78,28 @@ def parse(xmlfile):
         except AttributeError:
             return ""
 
-    def getvat(tagname):
+    def iferror2getalt(tagname,tagname2,attrib,tagnamealt):
+
+        try:
+            return soup.find(tagname).find(tagname2).get(attrib)
+        except AttributeError:
+            try:
+                return soup.find(tagnamealt).find(tagname2).get(attrib)
+            except AttributeError:
+                return ""
+
+    def getvat(tagname1,tagname2):
     
         vat = []
 
         try:
-            soup.find(tagname).find("value_cost").find("excluding_vat")
+            soup.find(tagname1).find(tagname2).find("value_cost").find("excluding_vat")
             vat = "excl"
         except AttributeError:
             vat = ""
 
         try:
-            soup.find(tagname).find("value_cost").find("including_vat")
+            soup.find(tagname1).find(tagname2).find("value_cost").find("including_vat")
             if vat == "excl":
                 pass
             else:
@@ -67,7 +108,7 @@ def parse(xmlfile):
                 vat = ""
 
         try: 
-            percentage = soup.find(tagname).find("vat_prct").get("fmtval")
+            percentage = soup.find(tagname1).find(tagname2).find("vat_prct").get("fmtval")
         except AttributeError:
             percentage = ""
 
@@ -97,6 +138,13 @@ def parse(xmlfile):
         except AttributeError:
             return ""
 
+    def toeuro(value,currency):
+
+        try:
+            eur = float(value) / float(exchangerates[currency])
+            return round(eur, 2)
+        except (AttributeError, KeyError, ValueError, TypeError):
+            return ""
 
     soup = bs(xmlfile)
 
@@ -107,6 +155,9 @@ def parse(xmlfile):
     # NO attrs
     if doc_type != "7":
         return
+
+#    else:
+#        print "score! "+url
 
     #contract attrs
     doc_number = iferror1("no_doc_ojs")
@@ -125,16 +176,19 @@ def parse(xmlfile):
     notice_dispatch_month = iferror2("notice_dispatch_date","month")
     notice_dispatch_year = iferror2("notice_dispatch_date","year")
 
-    contract_value = iferror2num("values_list","value")
-    contract_currency = iferror2get("values_list","value", "currency")
+    tender_value = iferror2num("values_list","value")
+    tender_currency = iferror2get("values_list","value", "currency")
+    tender_value_eur = toeuro(tender_value,tender_currency)
+    tender_value_vat,tender_value_vat_percent = getvat("total_final_value","costs_range_and_currency_with_vat_rate")
     
     # auth attrs
-    authority_name = iferror2("contacting_authority_information","organisation")
-    authority_address = iferror2("contacting_authority_information","address")
-    authority_town = iferror2("contacting_authority_information","town")
-    authority_postal_code = iferror2("contacting_authority_information","postal_code")
-    authority_country = iferror2get("contacting_authority_information","country","value")
-    authority_contact_person = iferror2("contacting_authority_information","attention")
+    authority_name = iferror2alt("contacting_authority_information","organisation","contracting_authority_information_contract_award","officialname")
+    authority_name_slug = slugify(authority_name)
+    authority_address = iferror2alt("contacting_authority_information","address","contracting_authority_information_contract_award","address")
+    authority_town = iferror2alt("contacting_authority_information","town","contracting_authority_information_contract_award","town")
+    authority_postal_code = iferror2alt("contacting_authority_information","postal_code","contracting_authority_information_contract_award","postal_code")
+    authority_country = iferror2getalt("contacting_authority_information","country","value","contracting_authority_information_contract_award")
+    authority_contact_person = iferror2alt("contacting_authority_information","attention","contracting_authority_information_contract_award","attention")
     authority_www = iferror2("internet_addresses_contract_award", "url_buyer")
     authority_type = iferror1get("type_of_contracting_authority", "value")
 
@@ -170,18 +224,23 @@ def parse(xmlfile):
         contract_award_year = iferror2("contract_award_date","year")
 
         company_name = iferror2("economic_operator_name_address","organisation")
+        company_name_slug = slugify(company_name)
         company_address = iferror2("economic_operator_name_address","address")
         company_town = iferror2("economic_operator_name_address","town")
         company_postal_code = iferror2("economic_operator_name_address","postal_code")
         company_country = iferror2get("economic_operator_name_address","country","value")
 
         initial_value = iferror2get("initial_estimated_total_value_contract","value_cost","fmtval")
-        initial_value_vat,initial_value_vat_percent = getvat("initial_estimated_total_value_contract")
+        initial_value_currency = iferror1get("initial_estimated_total_value_contract","currency")
+        initial_value_eur = toeuro(initial_value,initial_value_currency)
+        initial_value_vat,initial_value_vat_percent = getvat("contract_value_information","initial_estimated_total_value_contract")
         
-        total_value = iferror2get("total_final_value","value_cost","fmtval")
-        total_value_vat,total_value_vat_percent = getvat("total_final_value")
+        contract_value = iferror3get("contract_value_information","costs_range_and_currency_with_vat_rate","value_cost","fmtval")
+        contract_currency = iferror2get("contract_value_information","costs_range_and_currency_with_vat_rate","currency")
+        contract_value_eur = toeuro(contract_value,contract_currency)
+        contract_value_vat,contract_value_vat_percent = getvat("contract_value_information","costs_range_and_currency_with_vat_rate")
 
-        contract.extend([notice_dispatch_year,notice_dispatch_month,notice_dispatch_day,doc_number,doc_type,contract_type,proc_type,regulation_type,bid_type,award_criteria_type,framework,eu_project,GPA,country,NUTS,cpv,contract_number,contract_id,title_contract,short_contract_description,additional_info,contract_value,contract_currency,authority_name,authority_address,authority_town,authority_postal_code,authority_country,authority_contact_person,authority_www,authority_type,auth_type,contract_title,lot,nr_bids,contract_award_day,contract_award_month,contract_award_year,company_name,company_address,company_town,company_postal_code,company_country,initial_value,initial_value_vat_percent,total_value,total_value_vat_percent,url])
+        contract.extend([notice_dispatch_year,notice_dispatch_month,notice_dispatch_day,doc_number,doc_type,contract_type,proc_type,regulation_type,bid_type,award_criteria_type,framework,eu_project,GPA,country,NUTS,cpv,contract_number,contract_id,title_contract,short_contract_description,additional_info,tender_value,tender_currency,tender_value_eur,tender_value_vat,tender_value_vat_percent,authority_name,authority_name_slug,authority_address,authority_town,authority_postal_code,authority_country,authority_contact_person,authority_www,authority_type,auth_type,contract_title,lot,nr_bids,contract_award_day,contract_award_month,contract_award_year,company_name,company_name_slug,company_address,company_town,company_postal_code,company_country,initial_value,initial_value_currency,initial_value_eur,initial_value_vat,initial_value_vat_percent,contract_value,contract_currency,contract_value_eur,contract_value_vat,contract_value_vat_percent,url])
         row.append(contract)
     
     if len(row) == 0:
@@ -190,23 +249,26 @@ def parse(xmlfile):
     
     return row
 
+#print parse(testfile)
+
 ######## get ready.. write!
 ######## use for end xml files in directory alltar
 
-missing = open("TED-contracts-2011/missing_tenders.csv", "w") #catch xmls with different structure
+missing = open("TED-contracts-2011/missing_tenders.csv", "w")
 
 for nr in xrange(1,253):
 
     filenr = str(nr).zfill(3)
     print filenr
+    lapseTime = datetime.now()
    
-    # initiate csv file and add header
+#    initiate csv file and add header
     outputfile = open("TED-contracts-2011/contracts"+filenr+".csv", "w")
     writer = csv.writer(outputfile)
 
     missingWriter = csv.writer(missing)
 
-    header = ["notice_dispatch_year","notice_dispatch_month","notice_dispatch_day","doc_number","doc_type","contract_type","proc_type","regulation_type","bid_type","award_criteria_type","framework","eu_project","GPA","country","NUTS","cpv","contract_number","contract_id","title_contract","short_contract_description","additional_info","contract_value","contract_currency","authority_name","authority_address","authority_town","authority_postal_code","authority_country","authority_contact_person","authority_www","authority_type","auth_type","contract_title","lot","nr_bids","contract_award_day","contract_award_month","contract_award_year","company_name","company_address","company_town","company_postal_code","company_country","initial_value","initial_value_vat_percent","total_value","total_value_vat_percent","url"]
+    header = ["notice_dispatch_year","notice_dispatch_month","notice_dispatch_day","doc_number","doc_type","contract_type","proc_type","regulation_type","bid_type","award_criteria_type","framework","eu_project","GPA","country","NUTS","cpv","contract_number","contract_id","title_contract","short_contract_description","additional_info","tender_value","tender_currency","tender_value_eur","tender_value_vat","tender_value_vat_percent","authority_name","authority_name_slug","authority_address","authority_town","authority_postal_code","authority_country","authority_contact_person","authority_www","authority_type","auth_type","contract_title","lot","nr_bids","contract_award_day","contract_award_month","contract_award_year","company_name","company_name_slug","company_address","company_town","company_postal_code","company_country","initial_value","initial_value_currency","initial_value_eur","initial_value_vat","initial_value_vat_percent","contract_value","contract_currency","contract_value_eur","contract_value_vat","contract_value_vat_percent","url"]
 
     writer.writerow(header)
 
@@ -217,7 +279,12 @@ for nr in xrange(1,253):
         if row == None:
             continue
         elif 'http' in row[0]:
+            print row
             missingWriter.writerow([row, f])
         else:
             writer.writerows(row)
+
+    print datetime.now() - lapseTime,' / ',datetime.now() - startTime
+
+print datetime.now() - startTime
 
